a03cda3eda44f1357f3a40e9fa878f5e95a400e9
robertocarlosmedina <robertocarlosmedina.dev@gmail.com>
2022-03-27 23:55:03 -0100

    implementation of the front-page

--- src/components/frontPage.js
+++ src/components/frontPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./frontPage.css";
@@ -6,30 +6,90 @@ import "./frontPage.css";
 import CodingWorkshopRafiki from "../assets/ilustrations/Coding-workshop-rafiki.png";
 
 function FrontPage() {
+  const [coordinators, setCoordinators] = useState([
+    {
+      name: "Roberto Medina",
+      email: "rmedina@uta.cv",
+      img_url: "",
+    },
+    {
+      name: "Nuno iohoqr",
+      email: "inuno@uta.cv",
+      img_url: "",
+    },
+    {
+      name: "Jorgue",
+      email: "inuno@uta.cv",
+      img_url: "",
+    },
+    {
+      name: "Carlaos kdfn",
+      email: "inuno@uta.cv",
+      img_url: "",
+    },
+  ]);
+
   return (
-    <main className='frontPage-conteiner'>
-      <Link to={"/"}>
-        <h1 className='app-tittle frontPage-tittle-ajust'>Workshop</h1>
-      </Link>
-      <div className='right-card-container'>
-        <div className='right-card'></div>
-      </div>
-      <img
-        className={`ilustration frontPage-ajust`}
-        src={CodingWorkshopRafiki}
-        alt={"Coding workshop"}
-      />
-      <div className='text-conteiner'>
-        <p className='mural-text'>
-          Be part of this <br />
-          programming event
-          <br />
-          <span>Limited registration, register now</span>
-          <br />
-          <button className='button registration-button'>Register</button>
+    <main className='frontPage'>
+      <section className='frontPage-conteiner'>
+        <Link to={"/"}>
+          <h1 className='app-tittle frontPage-tittle-ajust'>Workshop</h1>
+        </Link>
+        <div className='right-card-container'>
+          <div className='right-card'></div>
+        </div>
+        <img
+          className={`ilustration frontPage-ajust`}
+          src={CodingWorkshopRafiki}
+          alt={"Coding workshop"}
+        />
+        <div className='text-conteiner'>
+          <p className='mural-text'>
+            Be part of this <br />
+            programming event
+            <br />
+            <span className='mural-text-1'>
+              Limited registration, register now
+            </span>
+            <br />
+            <button className='button registration-button'>Register</button>
+          </p>
+        </div>
+      </section>
+      <section className='descritpion-conteiner'>
+        <h1 className='session-tittle'>Description</h1>
+        <p className='description-session-text'>
+          Limited registration, register now Limited registration, register now
+          Limited registration, register now Limited registration, register now
         </p>
-        
-      </div>
+      </section>
+      <section className='coordinator-conteiner'>
+        <h1 className='session-tittle'>Coordinators</h1>
+        <div className='coordinator-list-display'>
+          <ul className='coordinator-display'>
+            {coordinators.map((coordinator, i) => (
+              <li
+                key={i}
+                className={`coordinators-item ${i === 0 ? "firts-item" : ""}`}>
+                <div>
+                  <p>
+                    <div className='coordinator-img'></div>
+                  </p>
+                  <p>
+                    {coordinator.name} <br /> {coordinator.email}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* <footer className="footer-item">
+        <h1 className='session-tittle'>Contributors</h1>
+        <p className='description-session-text'>
+        A special thank you to
+        </p>
+        </footer> */}
+      </section>
     </main>
   );
 }
