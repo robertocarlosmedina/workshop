68a5e08ffddb744a41e8cc34356323e7f5eef62a
Roberto <robertocarlosmedina.dev@gmail.com>
2022-04-16 21:29:05 -0100

    Connecting Team registration form to the api routes to make the registration process.
    And also handling the none time member available execption.

--- src/components/userInterface/competition/competitionRegistration.js
+++ src/components/userInterface/competition/competitionRegistration.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import UICards from "../../uicards/uiCards";
-import Api from '../../../api/api';
+import Api from "../../../api/api";
 import TeamSpirit from "../../../assets/ilustrations/team-spirit-pana.png";
 
 import "./competitionRegistration.css";
@@ -24,8 +24,7 @@ function CompetitionRegistration() {
 
   useEffect(() => {
     try {
-      Api.get(`/user_register/availableTeamMembers`)
-      .then((res) => {
+      Api.get(`/user_register/availableTeamMembers`).then((res) => {
         const requestResponse = res.data;
         if (requestResponse.statusCode !== 200) {
           setLoadingRegistrationHandler({
@@ -34,7 +33,7 @@ function CompetitionRegistration() {
           });
         }
         if (requestResponse) {
-          setAvailableParticipants(requestResponse.data)
+          setAvailableParticipants(requestResponse.data);
           setLoadingRegistrationHandler({
             ...loadingRegistrationHandler,
             validCode: true,
@@ -49,6 +48,12 @@ function CompetitionRegistration() {
   function validateEmptyString(string) {
     return string !== "" ? true : false;
   }
+
+  const resetDefaultValues = () => {
+    setTeamMember({ value: "", valid: false });
+    setTeamName({ value: "", valid: false });
+    setPersonalCode({ value: "", valid: false });
+  };
 
   const teamNameFieldHandler = (event) => {
     const givenText = event.target.value;
@@ -69,83 +74,114 @@ function CompetitionRegistration() {
 
   const submitTeamRegistration = async () => {
     if (teamName.valid && personalCode.valid && teamMember.valid) {
-      console.log(
-        teamName.value,
-        personalCode.value,
-        teamMember.value
-      );
+      const data = {
+        team_name: teamName.value,
+        code_first_member: personalCode.value,
+        id_second_member: teamMember.value,
+      };
+      console.log(data);
+      try {
+        Api.post("/team/create_team", data).then((res) => {
+          const newRegistre = res.data;
+          if (newRegistre.statusCode !== 200) {
+            console.log(newRegistre.errorMessage);
+            return;
+          }
+          if (newRegistre) {
+            console.log(newRegistre);
+            resetDefaultValues();
+          }
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
     }
-  }
+  };
 
   return (
-    <UICards card_name="ui-display-card">
+    <UICards card_name='ui-display-card'>
       <img
         style={{ animation: "top-bottom-animation 2s both" }}
         className={`ilustration ilustration-ajust`}
         src={TeamSpirit}
-        alt="Team spirit ilustration"
+        alt='Team spirit ilustration'
       />
-      <p className="normal-text competition-text bold-text">Sign up your team</p>
-      <form className="formulary">
-        <ul className="registration-form team-registraion-form">
-          <li className="form-item">
-            <p>
-              <label>
-                <span>*</span> Team Name
-              </label>
-            </p>
-            <input
-              type="text"
-              value={teamName.value}
-              onChange={teamNameFieldHandler}
-              className={`resgistrion-input-field ${
-                teamName.valid ? "valid" : "invalid"
-              }`}
-              required
-              placeholder="Example coders..."
-            />
-          </li>
-          <li className="form-item">
-            <p>
-              <label>
-                <span>*</span> Your Code
-              </label>
-            </p>
-            <input
-              type="text"
-              value={personalCode.value}
-              onChange={personalCodeFieldHandler}
-              className={`resgistrion-input-field ${
-                personalCode.valid ? "valid" : "invalid"
-              }`}
-              required
-              placeholder="Your personal code..."
-            />
-          </li>
-          <li className="form-item">
-            <p>
-              <label>
-                <span>*</span> Your Team Member
-              </label>
-            </p>
-            <select
-              onChange={teamMemberFieldHandler}
-              className="resgistrion-input-field  selector"
-            >
-              {availableParticipants.map((participant, i) => (
-                <option key={i} value={participant.participantId}>
-                  {participant.name}
-                </option>
-              ))}
-            </select>
-          </li>
-        </ul>
-      </form>
-      <button
+      <p className='normal-text competition-text bold-text'>
+        Sign up your team
+      </p>
+      {availableParticipants.length > 1 && (
+        <form className='formulary'>
+          <ul className='registration-form team-registraion-form'>
+            <li className='form-item'>
+              <p>
+                <label>
+                  <span>*</span> Team Name
+                </label>
+              </p>
+              <input
+                type='text'
+                value={teamName.value}
+                onChange={teamNameFieldHandler}
+                className={`resgistrion-input-field ${
+                  teamName.valid ? "valid" : "invalid"
+                }`}
+                required
+                placeholder='Example coders...'
+              />
+            </li>
+            <li className='form-item'>
+              <p>
+                <label>
+                  <span>*</span> Your Code
+                </label>
+              </p>
+              <input
+                type='text'
+                value={personalCode.value}
+                onChange={personalCodeFieldHandler}
+                className={`resgistrion-input-field ${
+                  personalCode.valid ? "valid" : "invalid"
+                }`}
+                required
+                placeholder='Your personal code...'
+              />
+            </li>
+            <li className='form-item'>
+              <p>
+                <label>
+                  <span>*</span> Your Team Member
+                </label>
+              </p>
+              <select
+                onChange={teamMemberFieldHandler}
+                className='resgistrion-input-field  selector'>
+                {availableParticipants.map((participant, i) => (
+                  <option key={i} value={participant.participantId}>
+                    {participant.name}
+                  </option>
+                ))}
+              </select>
+            </li>
+          </ul>
+        </form>
+      )}
+
+      {availableParticipants.length > 1 && (
+        <button
           className='button primary-button competition-ajust-button'
           onClick={submitTeamRegistration}>
           Register team
         </button>
+      )}
+      {availableParticipants.length < 1 && (
+        <div className='no-member-available'>
+          <p>
+            No members available!!
+            <br />
+            All teams have already been formed.
+          </p>
+        </div>
+      )}
     </UICards>
   );
 }
